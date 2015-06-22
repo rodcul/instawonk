abbee5ff77505964a16d3443c7e378afd2156efb
Alex Handy <handy437@gmail.com>
2015-06-22 15:52:50 +0100

    all insta users sorted by follower count (highest first)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.all.order(:followed_by).reverse
   end
 
   # GET /users/1
