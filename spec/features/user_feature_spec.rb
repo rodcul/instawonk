6deb7b95a3ee8c34285c68bc2cbf06feac025ed9
Ashleigh Maund <ashleighmaund@gmail.com>
2015-06-22 21:19:39 +0100

    added feature test - added search form but not adding to user model until amendments from previous branch are merged with master

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -14,7 +14,7 @@ feature 'User' do
                 followed_by: '1000000001')
   end
   scenario 'visits homepage and sees user details' do
-    visit('/users')
+    visit('/')
     expect(page).to have_content('AhIsCool')
     expect(page).to have_content('Alex Handy')
     expect(page).to have_content('Cooler than you megalolz')
@@ -23,14 +23,24 @@ feature 'User' do
   end
 
   scenario 'users are sorted by "followed by"' do
-    visit '/users'
+    visit '/'
     within('table.table tbody tr:nth-child(1)') do
       expect(page).to have_content 'Ashleigh'
     end
   end
 
   scenario 'can follow a link to the user\'s instagram page' do
-    visit '/users'
+    visit '/'
     expect(page).to have_selector "a[href='https://instagram.com/AhIsCool']"
+  end
+
+  scenario 'can search a term and will output a user with that term in bio' do
+    visit '/'
+    fill_in 'Search', with: 'coolest'
+    click_button 'Search'
+    expect(page).to have_content 'The coolest everrrr'
+    expect(page).to have_content 'Ashleigh'
+    expect(page).not_to have_content 'Cooler than you megalolz'
+    expect(page).not_to have_content 'Alex Handy'
   end
 end
