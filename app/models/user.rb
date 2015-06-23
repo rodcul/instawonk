57a53d1bb5039d7592f886ce7d56846bb9636bb4
Rodney Cullen <rodcul@gmail.com>
2015-06-23 14:21:24 +0100

    Allowed NULL on username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :username, uniqueness: true
+  validates :username, uniqueness: true, allow_nil: true
 
   def self.search(search_term)
     where("bio like ?", "%#{search_term}%")
