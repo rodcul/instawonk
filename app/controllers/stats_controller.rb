fe732a829f42f33857ede54fded7ba30f46078f6
Rodney Cullen <rodcul@gmail.com>
2015-06-23 23:29:12 +0100

    Changed variable name

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,5 @@
 class StatsController < ApplicationController
   def index
-    @users = User.where().not(username: nil).where(created_at: (Time.now - 24.hours)..Time.now)
+    @users_last_24h = User.where().not(username: nil).where(created_at: (Time.now - 24.hours)..Time.now)
   end
 end
