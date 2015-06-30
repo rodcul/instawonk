46adf66af884e3120c7ac636b0dbfa8f0cd02093
Rodney Cullen <rodcul@gmail.com>
2015-06-30 10:22:42 +0100

    Changed date for checked_instagram

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,6 +1,6 @@
 class StatsController < ApplicationController
   def index
     @user_count = User.where.not(username: nil).count
-    @users_last_24h = User.where().not(username: nil).where(created_at: (Time.now - 24.hours)..Time.now)
+    @users_last_24h = User.where().not(username: nil).where(checked_instagram: (Time.now - 24.hours)..Time.now)
   end
 end
