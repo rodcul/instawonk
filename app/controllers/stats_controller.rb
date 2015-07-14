5dc22390e4b8658b6e1f4d480efbd4f0cc9c6d49
Rodney Cullen <rodcul@gmail.com>
2015-07-14 08:46:04 +0100

    Updated stats to use checked_instagram

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,8 +1,8 @@
 class StatsController < ApplicationController
   def index
-    @user_count = User.where().not(username: nil).count
+    @user_count = User.where().not(username: nil, checked_instagram: nil).count
     @user_count_no_username = User.where(username: nil, checked_instagram: nil).count
-    @users_last_24h = User.where().not(username: nil).where(created_at: (Time.now - 24.hours)..Time.now).group_by_hour_of_day(:checked_instagram).count
-    @users_last_90d = User.where().not(username: nil).where(created_at: (Time.now - 90.days)..Time.now).group_by_day(:checked_instagram).count
+    @users_last_24h = User.where().not(username: nil).where(checked_instagram: (Time.now - 24.hours)..Time.now).group_by_hour_of_day(:checked_instagram).count
+    @users_last_90d = User.where().not(username: nil).where(checked_instagram: (Time.now - 90.days)..Time.now).group_by_day(:checked_instagram).count
   end
 end
