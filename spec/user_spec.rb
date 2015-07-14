cd90c21d058205ba95e5cdf1807b692bbf105152
Rodney Cullen <rodcul@gmail.com>
2015-07-14 15:40:55 +0100

    Updated travis.yml to work

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 describe User do
-  xit {should validate_uniqueness_of(:username)}
+  it {should validate_uniqueness_of(:username)}
   it {should validate_uniqueness_of(:instagram_id)}
 end
